d7799d2bfecf0ffffbb42ebcce7d1cf02198f244
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-08-17 13:30:51 -0400

    Final Project compressed using npm build

---
+++ build/precache-manifest.325edf3f5a7b1239111e0c694d4d5c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89223d9b8d51a52fa26545de8dc4fc5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a42b2ed66e5d612cb747",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e449c7e8c33c9eec7f95",
+    "url": "/static/css/main.dc2a11e8.chunk.css"
+  },
+  {
+    "revision": "a42b2ed66e5d612cb747",
+    "url": "/static/js/2.855ee9cb.chunk.js"
+  },
+  {
+    "revision": "e449c7e8c33c9eec7f95",
+    "url": "/static/js/main.7e9e6aeb.chunk.js"
+  },
+  {
+    "revision": "07b927413151ab70847a",
+    "url": "/static/js/runtime~main.3338ace7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
